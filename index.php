95a6ace526adc46924667ce1ab3c876912585d1d
matequin <matequin10@hotmail.com>
2021-10-12 15:42:09 -0500

    se agrego la estructura basica de html

---
+++ index.php
@@ -0,0 +1,5 @@
+<meta property="og:site_name" content="Provócame">
+<meta property="og:url" content="https://provocame.com/cart">
+<meta property="og:title" content="Tu carrito">
+<meta property="og:type" content="website">
+<meta property="og:description" content="provocame, pijamas sexy, multiusos, leggins, deportivos, vestidos, batolas, infantil, licencias disney, mickey, minnie, frozen, niños, niñas, baby doll, venta catálogo, tienda online, satín, satin.">
